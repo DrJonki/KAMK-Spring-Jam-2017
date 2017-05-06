e42f5f3b8cc219ee90a76d8d5a34dd38ac77d04f
Joona Tiinanen <joona_tiinanen@hotmail.com>
2017-05-07 01:54:37 +0300

    Timer

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour {
 
     private PlayerController m_playerController;
     private EnemyController m_enemyController;
+    private Text m_timeTextComponent;
+    private float m_timer = 0f;
     
 	void Start ()
     {
         m_playerController = GameObject.Find("Player").GetComponent<PlayerController>();
         m_enemyController = GameObject.Find("Enemy").GetComponent<EnemyController>();
+        m_timeTextComponent = GameObject.Find("TimeText").GetComponent<Text>();
     }
 	
 	void Update ()
@@ -21,5 +25,7 @@ public class SceneController : MonoBehaviour {
             Debug.Log("Defeat");
             SceneManager.LoadScene("FailScene");
         }
+
+        m_timeTextComponent.text = "Time: " + (m_timer += Time.deltaTime).ToString("0.0");
 	}
 }
