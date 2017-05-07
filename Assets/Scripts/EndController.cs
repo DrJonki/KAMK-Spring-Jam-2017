5b70d1dc2129b87b22dcccd27a1ca653496c7fc9
Joona Tiinanen <joona_tiinanen@hotmail.com>
2017-05-07 15:18:12 +0300

    Sounds

---
+++ Assets/Scripts/EndController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndController : MonoBehaviour {
+
+    public AudioSource ohYeah;
+    public AudioSource siren;
+    public AudioSource ohShit;
+
+    // Use this for initialization
+    void Start () {
+        ohYeah.Play();
+        siren.PlayDelayed(0.8f);
+        ohShit.PlayDelayed(3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
